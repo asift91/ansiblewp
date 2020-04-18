77e4aba18beacae7c05698558b763f2d5f6b075b
unknown <asift@italentdigital.com>
2020-04-18 11:37:53 +0530

    added sudo for copy command

--- install_wp.sh
+++ install_wp.sh
@@ -8,29 +8,23 @@ echo "dbusername is : ${5}" >> /home/${3}/var.txt
 echo "dbPassword is : ${6}" >> /home/${3}/var.txt
 
 ssh_key_configuration() {
-#sudo 
-apt-get install sshpass
-#sudo 
-ssh-keygen -t rsa -N '' -f /home/${3}/.ssh/id_rsa <<< y
-  echo "${2}" | sshpass ssh-copy-id -f -i /home/${3}/.ssh/id_rsa.pub ${3}@"${1}"
- #sudo chown ${3}:${3} /home/${3}/.ssh/id_rsa*
+sudo apt-get install sshpass
+sudo ssh-keygen -t rsa -N '' -f /home/${3}/.ssh/id_rsa <<< y
+sudo  echo "${2}" | sshpass ssh-copy-id -f -i /home/${3}/.ssh/id_rsa.pub ${3}@"${1}"
+sudo chown ${3}:${3} /home/${3}/.ssh/id_rsa*
 }
 
 install_ansible() {
-#sudo 
-apt-add-repository ppa:ansible/ansible -y
-#sudo 
-apt-get update
-#sudo 
-apt-get install ansible -y
+sudo apt-add-repository ppa:ansible/ansible -y
+sudo apt-get update
+sudo apt-get install ansible -y
 }
 
 configure_ansible() {
-#sudo chown -R ${2}:${2} /home/${2}/.ansible/cp
+sudo chown -R ${2}:${2} /home/${2}/.ansible/cp
 echo "Configure ansible Ip is : ${1}" >> /home/${2}/var.txt
-#sudo chmod 777 /etc/ansible/hosts
-#sudo 
-echo -e "[webservers]\n${1}" >>/etc/ansible/hosts
+sudo chmod 777 /etc/ansible/hosts
+sudo echo -e "[webservers]\n${1}" >>/etc/ansible/hosts
 }
 
 wordpress_install() {
@@ -40,14 +34,10 @@ echo "username is : ${1}" >> /home/${1}/var.txt
 echo "dbservername is : ${2}" >> /home/${1}/var.txt
 echo "dbusername is : ${3}" >> /home/${1}/var.txt
 echo "dbPassword is : ${4}" >> /home/${1}/var.txt
-#sudo 
-sed -i "s~wp_db_name: wordpress~wp_db_name: ${2}~" /home/${1}/wordpressplaybook/group_vars/all  >> /home/${1}/var.txt
-#sudo 
-sed -i "s~wp_db_user: wordpress~wp_db_user: ${3}~" /home/${1}/wordpressplaybook/group_vars/all  >> /home/${1}/var.txt 
-#sudo 
-sed -i "s~wp_db_password: password~wp_db_password: ${4}~" /home/${1}/wordpressplaybook/group_vars/all  >> /home/${1}/var.txt 
-#sudo 
-sed -i "s~#   StrictHostKeyChecking ask~   StrictHostKeyChecking no~" /etc/ssh/ssh_config  >> /home/${1}/var.txt
+sudo sed -i "s~wp_db_name: wordpress~wp_db_name: ${2}~" /home/${1}/wordpressplaybook/group_vars/all  >> /home/${1}/var.txt
+sudo sed -i "s~wp_db_user: wordpress~wp_db_user: ${3}~" /home/${1}/wordpressplaybook/group_vars/all  >> /home/${1}/var.txt 
+sudo sed -i "s~wp_db_password: password~wp_db_password: ${4}~" /home/${1}/wordpressplaybook/group_vars/all  >> /home/${1}/var.txt 
+sudo sed -i "s~#   StrictHostKeyChecking ask~   StrictHostKeyChecking no~" /etc/ssh/ssh_config  >> /home/${1}/var.txt
 ansible-playbook /home/${1}/wordpressplaybook/playbook.yml -i /etc/ansible/hosts -u ${1}
 }
 
@@ -55,6 +45,5 @@ ssh_key_configuration ${1} ${2} ${3} >> /home/${3}/var.txt
 install_ansible >> /home/${3}/var.txt
 configure_ansible ${1} ${3} >> /home/${3}/var.txt
 wordpress_install ${3} ${4} ${5} ${6} >> /home/${3}/var.txt
-#sudo sed -i "s~   StrictHostKeyChecking no~#   StrictHostKeyChecking ask~" /etc/ssh/ssh_config  >> /home/${3}/var.txt
-#sudo 
-systemctl restart ssh
+sudo sed -i "s~   StrictHostKeyChecking no~#   StrictHostKeyChecking ask~" /etc/ssh/ssh_config  >> /home/${3}/var.txt
+sudo systemctl restart ssh
