26fafcc05ff1296b9e4762e38618cac779b9aea8
unknown <asift@italentdigital.com>
2020-04-21 10:13:27 +0530

    modified script 21/04/2020

--- install_wp.sh
+++ install_wp.sh
@@ -8,7 +8,6 @@ echo "dbPassword is : ${6}" >> /home/${3}/var.txt
 
 ssh_key_configuration() {
 sudo apt-get update  >> /home/${3}/var.txt
-apt-cache search sshpass  >> /home/${3}/var.txt
 echo "---------------------------------------------------------------------------" >> /home/${3}/var.txt
 sudo apt-get install sshpass  >> /home/${3}/var.txt
 apt-cache search sshpass  >> /home/${3}/var.txt
@@ -18,11 +17,6 @@ echo "--------------------------------------------------------------------------
 echo "Changing permissions for id_rsa" >> /home/${3}/var.txt
  sudo chown ${3}:${3} /home/${3}/.ssh/id_rsa* >> /home/${3}/var.txt
 echo "---------------------------------------------------------------------------"
-echo "---------------------------------------------------------------------------"
-echo "Public Ip is : ${1}" >> /home/${3}/var.txt
-echo "Password is : ${2}" >> /home/${3}/var.txt
-echo "username is : ${3}" >> /home/${3}/var.txt
-echo "---------------------------------------------------------------------------"
 echo "before ssh-copy-id command" >> /home/${3}/var.txt
 echo "---------------------------------------------------------------------------"
  # echo "${2}" | sshpass ssh-var-id -f -i /home/${3}/.ssh/id_rsa.pub ${3}@${1} >> /home/${3}/var.txt
@@ -30,12 +24,12 @@ sudo sshpass -p "${2}" ssh-copy-id -i /home/${3}/.ssh/id_rsa.pub ${3}@${1} >> /h
 echo "---------------------------------------------------------------------------"
 echo "after ssh-copy-id command" >> /home/${3}/var.txt
 echo "---------------------------------------------------------------------------"
-echo "---------------------------------------------------------------------------"
 }
 
 install_ansible() {
-sudo apt-add-repository ppa:ansible/ansible -y
-sudo apt-get update
+sudo apt update
+sudo apt install software-properties-common -y
+sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt-get install ansible -y
 }
 
@@ -44,13 +38,13 @@ echo "Configure ansible Ip is : ${1}" >> /home/${2}/var.txt
 sudo sed -i "s~#   StrictHostKeyChecking ask~   StrictHostKeyChecking no~" /etc/ssh/ssh_config  >> /home/${3}/var.txt
 sudo chmod 777 /etc/ansible/hosts
 sudo echo -e "[webservers]\n${1}" >>/etc/ansible/hosts
-ansible -m ping all -u ${1} >> /home/${2}/var.txt
-sudo chown -R ${2}:${2} /home/${2}/.ansible/cp
+#sudo chown -R ${2}:${2} /home/${2}/.ansible/cp
 }
 
 wordpress_install() {
-cd /home/${1}
+#cd /home/${1}
 git clone https://github.com/sayosh0512/wordpressplaybook.git
+sudo chown -R ${1}:${1} /home/${1}/wordpressplaybook
 echo "username is : ${1}" >> /home/${1}/var.txt
 echo "dbservername is : ${2}" >> /home/${1}/var.txt
 echo "dbusername is : ${3}" >> /home/${1}/var.txt
@@ -58,11 +52,12 @@ echo "dbPassword is : ${4}" >> /home/${1}/var.txt
 sudo sed -i "s~wp_db_name: wordpress~wp_db_name: ${2}~" /home/${1}/wordpressplaybook/group_vars/all  >> /home/${1}/var.txt
 sudo sed -i "s~wp_db_user: wordpress~wp_db_user: ${3}~" /home/${1}/wordpressplaybook/group_vars/all  >> /home/${1}/var.txt 
 sudo sed -i "s~wp_db_password: password~wp_db_password: ${4}~" /home/${1}/wordpressplaybook/group_vars/all  >> /home/${1}/var.txt 
-ansible-playbook /home/${1}/wordpressplaybook/playbook.yml -i /etc/ansible/hosts -u ${1}
+ansible -m ping all -u ${1} >>  /home/${1}/var.txt
+ansible-playbook /home/${1}/wordpressplaybook/playbook.yml -i /etc/ansible/hosts -u ${1} >>  /home/${1}/var.txt
 }
 
-sudo sed -i "s~#   StrictHostKeyChecking ask~   StrictHostKeyChecking no~" /etc/ssh/ssh_config  >> /home/${3}/var.txt
-sudo systemctl restart ssh
+#sudo sed -i "s~#   StrictHostKeyChecking ask~   StrictHostKeyChecking no~" /etc/ssh/ssh_config  >> /home/${3}/var.txt
+#sudo systemctl restart ssh
 ssh_key_configuration ${1} ${2} ${3} >> /home/${3}/var.txt
 install_ansible >> /home/${3}/var.txt
 configure_ansible ${1} ${3} >> /home/${3}/var.txt
