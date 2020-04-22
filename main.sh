26087c1879f12a3efd269d5d577491aadb3e6fd7
unknown <asift@italentdigital.com>
2020-04-22 16:20:57 +0530

    added only admin parameter and removed other parameters

--- main.sh
+++ main.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-echo "Public Ip is : ${1}" >> log.txt
-echo "Password is : ${2}" >> log.txt
-echo "username is : ${3}" >> log.txt
-echo "dbservername is : ${4}" >> log.txt
-echo "dbusername is : ${5}" >> log.txt
-echo "dbPassword is : ${6}" >> log.txt
+echo "User ID is : ${1}" >> log.txt
 
 clonerepo(){
-cd /home/${3}/ 
+cd /home/${1}/ 
 git clone https://github.com/asift91/ansiblewp.git  
-sudo chown -R ${3}:${3} /home/{3}/ansiblewp
+sudo chown -R ${1}:${1} /home/{1}/ansiblewp
 }
 
-clonerepo ${1} ${2} ${3} ${4} ${5} ${6} >> log.txt
+clonerepo ${1} >> log.txt
