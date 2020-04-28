2eea9797ca492f74b0d400377b749821ab26dde1
unknown <asift@italentdigital.com>
2020-04-28 13:07:20 +0530

    added sudo

--- testwp.sh
+++ testwp.sh
@@ -8,15 +8,15 @@ change_location() {
 configuring_certs() {
     echo "certs func"
     sudo mkdir /azlamp/certs/wptest.com
-    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /azlamp/certs/wptest.com/nginx.key -out /azlamp/certs/wptest.com/nginx.crt -subj "/C=US/ST=WA/L=Redmond/O=IT/CN=wptest.com"
-    chown www-data:www-data /azlamp/certs/wptest.com/nginx.*
-    chmod 400 /azlamp/certs/wptest.com/nginx.*
+    sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /azlamp/certs/wptest.com/nginx.key -out /azlamp/certs/wptest.com/nginx.crt -subj "/C=US/ST=WA/L=Redmond/O=IT/CN=wptest.com"
+    sudo chown www-data:www-data /azlamp/certs/wptest.com/nginx.*
+    sudo chmod 400 /azlamp/certs/wptest.com/nginx.*
 
 }
 linking_data_location() {
     echo "linking func"
     sudo mkdir -p /azlamp/data/wptest.com/wp-content/uploads
-    ln -s /azlamp/data/wptest.com/wp-content/uploads /azlamp/html/wptest.com/wp-content/uploads
+    sudo ln -s /azlamp/data/wptest.com/wp-content/uploads /azlamp/html/wptest.com/wp-content/uploads
     sudo chmod 0777 /azlamp/data/wptest.com/wp-content/uploads
 }
 update_nginx_configuration() {
@@ -26,8 +26,8 @@ update_nginx_configuration() {
     sudo sed -i "s~#    . /azlamp/bin/utils.sh~   . /azlamp/bin/utils.sh~" /azlamp/bin/update-vmss-config
     sudo sed -i "s~#    reset_all_sites_on_vmss true VMSS~    reset_all_sites_on_vmss true VMSS~" /azlamp/bin/update-vmss-config
     sudo sed -i "s~#;;~;;~" /azlamp/bin/update-vmss-config
-    echo "sleep for 60 seconds"
-    sleep 60
+    #echo "sleep for 60 seconds"
+    sleep 30
 }
 replication() {
     echo "replication func"
