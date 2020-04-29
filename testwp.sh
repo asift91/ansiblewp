5a420ebb230d0f1ab3d002f3ccd72114c70e1b5f
asift91 <60592188+asift91@users.noreply.github.com>
2020-04-29 17:25:40 +0530

    Update testwp.sh

--- testwp.sh
+++ testwp.sh
@@ -2,22 +2,22 @@
 
 change_location() {
     echo "change locationfunction"
-    sudo mkdir /azlamp/html/wptest.com
-    sudo cp -rf /var/www/html/wordpress/* /azlamp/html/wptest.com
+    sudo mkdir /azlamp/html/${1}
+    sudo cp -rf /var/www/html/wordpress/* /azlamp/html/${1}
 }
 configuring_certs() {
     echo "certs func"
-    sudo mkdir /azlamp/certs/wptest.com
-    sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /azlamp/certs/wptest.com/nginx.key -out /azlamp/certs/wptest.com/nginx.crt -subj "/C=US/ST=WA/L=Redmond/O=IT/CN=wptest.com"
-    sudo chown www-data:www-data /azlamp/certs/wptest.com/nginx.*
-    sudo chmod 400 /azlamp/certs/wptest.com/nginx.*
+    sudo mkdir /azlamp/certs/${1}
+    sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /azlamp/certs/${1}/nginx.key -out /azlamp/certs/${1}/nginx.crt -subj "/C=US/ST=WA/L=Redmond/O=IT/CN=wptest.com"
+    sudo chown www-data:www-data /azlamp/certs/${1}/nginx.*
+    sudo chmod 400 /azlamp/certs/${1}/nginx.*
 
 }
 linking_data_location() {
     echo "linking func"
-    sudo mkdir -p /azlamp/data/wptest.com/wp-content/uploads
-    sudo ln -s /azlamp/data/wptest.com/wp-content/uploads /azlamp/html/wptest.com/wp-content/uploads
-    sudo chmod 0777 /azlamp/data/wptest.com/wp-content/uploads
+    sudo mkdir -p /azlamp/data/${1}/wp-content/uploads
+    sudo ln -s /azlamp/data/${1}/wp-content/uploads /azlamp/html/${1}/wp-content/uploads
+    sudo chmod 0777 /azlamp/data/${1}/wp-content/uploads
 }
 update_nginx_configuration() {
     echo "update nginx"
@@ -34,8 +34,10 @@ replication() {
     cd /usr/local/bin/
     sudo bash update_last_modified_time.azlamp.sh
 }
-change_location >>/home/azureadmin/copy.txt
-configuring_certs >>/home/azureadmin/copy.txt
-linking_data_location >>/home/azureadmin/copy.txt
+
+# ${1} value is a domain name which will update in runtime
+change_location ${1}>>/home/azureadmin/copy.txt
+configuring_certs ${1}>>/home/azureadmin/copy.txt
+linking_data_location ${1}>>/home/azureadmin/copy.txt
 update_nginx_configuration >>/home/azureadmin/copy.txt
 replication >>/home/azureadmin/copy.txt
