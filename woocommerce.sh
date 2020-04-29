8b80b6588f5913ae34ac5a6a1cd297682a93ba46
asift91 <60592188+asift91@users.noreply.github.com>
2020-04-29 15:57:10 +0530

    Update woocommerce.sh

--- woocommerce.sh
+++ woocommerce.sh
@@ -1,13 +1,11 @@
 #!bin/bash
 
-downloadwoocommerce()
-{
-wget -p /home/italent/https://downloads.wordpress.org/plugin/woocommerce.4.0.1.zip
+downloadwoocommerce(){
+  wget -p /home/italent/https://downloads.wordpress.org/plugin/woocommerce.4.0.1.zip
 }
 
-extractfile()
-{
-tar xvf woocommerce.4.0.1.zip -C /$7/wp-content/plugin
+extractfile(){
+  tar xvf woocommerce.4.0.1.zip -C /$7/wp-content/plugin
 }
 
 downloadwoocommerce
