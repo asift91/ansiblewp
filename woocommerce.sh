393bc31e1c284f415fca99ef7c2a101b63b46bed
sayosh0512 <63592135+sayosh0512@users.noreply.github.com>
2020-04-29 15:54:04 +0530

    Initial commit

--- woocommerce.sh
+++ woocommerce.sh
@@ -1,1 +1,15 @@
 #!bin/bash
+
+downloadwoocommerce()
+{
+wget -p /home/italent/https://downloads.wordpress.org/plugin/woocommerce.4.0.1.zip
+}
+
+extractfile()
+{
+tar xvf woocommerce.4.0.1.zip -C /$7/wp-content/plugin
+}
+
+downloadwoocommerce
+extractfile
+
