a5d8cafe62db793474a38cc742b3d88e2c655c3d
asift91 <60592188+asift91@users.noreply.github.com>
2020-04-30 13:23:24 +0530

    Update woocommerce.sh

--- woocommerce.sh
+++ woocommerce.sh
@@ -7,7 +7,9 @@ extractfile(){
   echo "domain_name ${1}" >>/home/azureadmin/log.txt
   sudo apt install unzip
   #cd /home/azureadmin/downloads.wordpress.org/plugin/
-  sudo unzip /home/azureadmin/downloads.wordpress.org/plugin/woocommerce.4.0.1.zip /var/www/html/wordpress/wp-content/plugin
+  sudo unzip /home/azureadmin/downloads.wordpress.org/plugin/woocommerce.4.0.1.zip
+  sudo cp -rf /home/azureadmin/woocommerce /var/www/html/wordpress/wp-content/plugins/
+  sudo rm -rf /home/azureadmin/woocommerce
 }
 downloadwoocommerce
 extractfile ${1}
