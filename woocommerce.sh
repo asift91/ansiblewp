40b450a4f2674bdf2bc083d17b5d9a80b175d917
sayosh0512 <63592135+sayosh0512@users.noreply.github.com>
2020-04-30 12:56:46 +0530

    Update woocommerce.sh

--- woocommerce.sh
+++ woocommerce.sh
@@ -2,16 +2,12 @@
 downloadwoocommerce(){
   wget -p https://downloads.wordpress.org/plugin/woocommerce.4.0.1.zip /home/azureadmin/
 }
-change_location(){
-    sudo mkdir /azlamp/html/${1}
-    sudo cp -rf /var/www/html/wordpress/* /azlamp/html/${1}
-}
+
 extractfile(){
   echo "domain_name ${1}" >>/home/azureadmin/log.txt
   sudo apt install unzip
   #cd /home/azureadmin/downloads.wordpress.org/plugin/
-  sudo unzip /home/azureadmin/downloads.wordpress.org/plugin/woocommerce.4.0.1.zip /var/www/html/${1}/wp-content/plugin
+  sudo unzip /home/azureadmin/downloads.wordpress.org/plugin/woocommerce.4.0.1.zip /var/www/html/wordpress/wp-content/plugin
 }
 downloadwoocommerce
-change_location ${1} 
 extractfile ${1}
