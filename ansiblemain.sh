cfac56cb96ca631e3ffbf73845b4f2ef5763bdec
asift91 <60592188+asift91@users.noreply.github.com>
2020-04-30 20:52:30 +0530

    Added run script code
    Added run script code

--- ansiblemain.sh
+++ ansiblemain.sh
@@ -2,10 +2,18 @@
 echo "User ID is : ${1}" >> log.txt
 
 clonerepo(){
-cd /home/${1}/ 
-wget https://raw.githubusercontent.com/sayosh0512/ansibles-wordpress-script/master/wordpressinstall.sh 
+  cd /home/${1}/ 
+  wget https://raw.githubusercontent.com/sayosh0512/ansibles-wordpress-script/master/wordpressinstall.sh 
+  sudo chown -R ${1}:${1} /home/${1}/wordpressinstall.sh
+}
 
-sudo chown -R ${1}:${1} /home/${1}/wordpressinstall.sh
+createrunscript(){
+  cat <<EOF > /home/${1}/run.sh
+  #!/bin/bash
+  bash /home/${1}/wordpressinstall.sh ${2}
+  EOF
+  sudo chown -R ${1}:${1} /home/${1}/run.sh
 }
 
 clonerepo ${1} >> log.txt
+createrunscript ${1} ${2}
