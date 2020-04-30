cadadc8c6cec85d148c673b30c07ee8cf44c0eec
sayosh0512 <63592135+sayosh0512@users.noreply.github.com>
2020-04-30 15:21:16 +0530

    Update ansiblemain.sh

--- ansiblemain.sh
+++ ansiblemain.sh
@@ -3,7 +3,7 @@ echo "User ID is : ${1}" >> log.txt
 
 clonerepo(){
 cd /home/${1}/ 
-wget https://raw.githubusercontent.com/sayosh0512/ansibles-wordpress-script/master/setup_ansible.sh  
+wget https://raw.githubusercontent.com/sayosh0512/ansibles-wordpress-script/master/wordpressinstall.sh 
 
 sudo chown -R ${1}:${1} /home/${1}/setup_ansible.sh
 }
