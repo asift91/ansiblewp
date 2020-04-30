9adb640fc59175bb2622590b1642d0cb93912382
sayosh0512 <63592135+sayosh0512@users.noreply.github.com>
2020-04-30 15:17:30 +0530

    Update ansiblemain.sh

--- ansiblemain.sh
+++ ansiblemain.sh
@@ -6,7 +6,6 @@ cd /home/${1}/
 wget https://raw.githubusercontent.com/sayosh0512/ansibles-wordpress-script/master/setup_ansible.sh  
 
 sudo chown -R ${1}:${1} /home/${1}/setup_ansible.sh
-#sudo chown -R azureadmin:azureadmin /home/azureadmin/install_wp.sh
 }
 
 clonerepo ${1} >> log.txt
