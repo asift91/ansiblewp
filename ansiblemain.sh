e080c4ffbb74561477086dc5e903d329f4b15b24
asift91 <60592188+asift91@users.noreply.github.com>
2020-05-01 13:12:08 +0530

    Changed EOF command
    Modified script 
    adjusted EOF command spacing

--- ansiblemain.sh
+++ ansiblemain.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-echo "User ID is : ${1}" >> log.txt
+
+echo "User ID is : ${1}" >>log.txt
+echo "Command is : ${2}" >> log.txt
 
 clonerepo(){
-  cd /home/${1}/ 
-  wget https://raw.githubusercontent.com/sayosh0512/ansibles-wordpress-script/master/wordpressinstall.sh 
-  sudo chown -R ${1}:${1} /home/${1}/wordpressinstall.sh
+  cd /home/"${1}"/ || exit
+  wget https://raw.githubusercontent.com/sayosh0512/ansibles-wordpress-script/master/wordpressinstall.sh
+  sudo chown -R "${1}":"${1}" /home/"${1}"/wordpressinstall.sh
 }
-
 createrunscript(){
-  cat <<EOF > /home/${1}/run.sh
+  cat <<EOF > /home/"${1}"/run.sh
   #!/bin/bash
   bash /home/${1}/wordpressinstall.sh ${2}
-  EOF
-  sudo chown -R ${1}:${1} /home/${1}/run.sh
+EOF
+sudo chown -R "${1}":"${1}" /home/"${1}"/run.sh
 }
-
 clonerepo ${1} >> log.txt
-createrunscript ${1} ${2}
+createrunscript ${1} "${2}" >> log.txt
