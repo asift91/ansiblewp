efc5ead15e1720204d3cb56ea52c1f54899a3d34
asift91 <60592188+asift91@users.noreply.github.com>
2020-05-04 20:16:36 +0530

    Update ansiblemain.sh

--- ansiblemain.sh
+++ ansiblemain.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-decotext=`echo '${2}' | base64 --decode`
+decotext=`echo ${2} | base64 --decode`
   
 echo "User ID is : ${1}" >> /home/"${1}"/log.txt
 echo "encoded text : ${2}" >> /home/"${1}"/log.txt
 echo "decoded text : ${decotext}" >> /home/"${1}"/log.txt
 
 clonerepo(){
-  cd /home/"${1}"/ || exit
+  cd /home/"${1}"/ 
   wget https://raw.githubusercontent.com/sayosh0512/ansibles-wordpress-script/master/wordpressinstall.sh
   sudo chown -R "${1}":"${1}" /home/"${1}"/wordpressinstall.sh
 }
