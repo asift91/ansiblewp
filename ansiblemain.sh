4e0c37b9bc9837392602139ce156c89c498dd8e4
asift91 <60592188+asift91@users.noreply.github.com>
2020-05-04 20:48:52 +0530

    Update ansiblemain.sh

--- ansiblemain.sh
+++ ansiblemain.sh
@@ -11,14 +11,12 @@ clonerepo(){
   wget https://raw.githubusercontent.com/sayosh0512/ansibles-wordpress-script/master/wordpressinstall.sh
   sudo chown -R "${1}":"${1}" /home/"${1}"/wordpressinstall.sh
 }
+clonerepo ${1} >> /home/"${1}"/log.txt
 
-createrunscript(){
   cat <<EOF > /home/"${1}"/run.sh
   #!/bin/bash
   bash /home/${1}/wordpressinstall.sh ${2}
 EOF
 sudo chown -R "${1}":"${1}" /home/"${1}"/run.sh
-}
 
-clonerepo ${1} >> /home/"${1}"/log.txt
 createrunscript ${1} ${decotext} >> /home/"${1}"/log.txt
