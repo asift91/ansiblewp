d3df04704f8224a60f782935272be47ad0808c54
asift91 <60592188+asift91@users.noreply.github.com>
2020-05-04 18:13:14 +0530

    Added decoded command
    Added a command to decode the encoded input and storing in one variable

--- ansiblemain.sh
+++ ansiblemain.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-decode_input=${2} | base64 --decode
+decotext=`echo '${2}' | base64 --decode`
+
 
 echo "User ID is : ${1}" >> /home/"${1}"/log.txt
-echo "Command is : ${decode_input}" >> /home/"${1}"/log.txt
+echo "encoded text : ${decotext} >> /home/"${1}"/log.txt
 
 clonerepo(){
   cd /home/"${1}"/ || exit
   wget https://raw.githubusercontent.com/sayosh0512/ansibles-wordpress-script/master/wordpressinstall.sh
   sudo chown -R "${1}":"${1}" /home/"${1}"/wordpressinstall.sh
 }
+
 createrunscript(){
   cat <<EOF > /home/"${1}"/run.sh
   #!/bin/bash
-  bash /home/${1}/wordpressinstall.sh ${2} | base64 --decode
+  bash /home/${1}/wordpressinstall.sh ${2}
 EOF
 sudo chown -R "${1}":"${1}" /home/"${1}"/run.sh
 }
+
 clonerepo ${1} >> /home/"${1}"/log.txt
-createrunscript ${1} ${2} >> /home/"${1}"/log.txt
+createrunscript ${1} ${decotext} >> /home/"${1}"/log.txt
