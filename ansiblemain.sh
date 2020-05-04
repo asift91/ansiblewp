238a62a63d8e72668b08abaaef7afd85341478b0
asift91 <60592188+asift91@users.noreply.github.com>
2020-05-04 20:02:04 +0530

    Update ansiblemain.sh

--- ansiblemain.sh
+++ ansiblemain.sh
@@ -4,8 +4,8 @@ decotext=`echo '${2}' | base64 --decode`
 
 
 echo "User ID is : ${1}" >> /home/"${1}"/log.txt
-echo "encoded text : ${2} >> /home/"${1}"/log.txt
-echo "decoded text : ${decotext} >> /home/"${1}"/log.txt
+echo "encoded text : ${2}" >> /home/"${1}"/log.txt
+echo "decoded text : ${decotext}" >> /home/"${1}"/log.txt
 
 clonerepo(){
   cd /home/"${1}"/ || exit
