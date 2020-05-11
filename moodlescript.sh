07c047edd42c5286460f337f33fa95e66f31c35d
sayosh0512 <63592135+sayosh0512@users.noreply.github.com>
2020-05-11 20:38:49 +0530

    Update moodlescript.sh

--- moodlescript.sh
+++ moodlescript.sh
@@ -22,9 +22,9 @@ moodle_install() {
   sudo sed -i "s~user_name: azusername~user_name: ${3}~" /home/${3}/ansible_playbook/group_vars/all  >> /home/${3}/var.txt
   sudo sed -i "s~vm_ip: IP~vm_ip: ${1}~" /home/${3}/ansible_playbook/group_vars/all  >> /home/${3}/var.txt
   sudo sed -i "s~vm_password: password~vm_password: ${2}~" /home/${3}/ansible_playbook/group_vars/all  >> /home/${3}/var.txt
-  sudo sed -i "s~dbhost: localhost~dbhost: ~” /home/${3}/ansible_playbook/roles/moodle/templates/config.php.j2 >> /home/${3}/var.txt
-sudo sed -i "s~dbuser: dbusername~dbuser: ~” /home/${3}/ansible_playbook/roles/moodle/templates/config.php.j2 >> /home/${3}/var.txt
-sudo sed -i "s~dbpass: dbpassword~dbpass: ~” /home/${3}/ansible_playbook/roles/moodle/templates/config.php.j2 >> /home/${3}/var.txt
+  sudo sed -i "s~dbhost: localhost~dbhost: ~” /home/${3}/ansible_playbook/group_vars/all >> /home/${3}/var.txt
+  sudo sed -i "s~dbuser: dbusername~dbuser: ~” /home/${3}/ansible_playbook/group_vars/all >> /home/${3}/var.txt
+  sudo sed -i "s~dbpass: dbpassword~dbpass: ~” /home/${3}/ansible_playbook/group_vars/all >> /home/${3}/var.txt
   ansible-playbook /home/${3}/ansible_playbook/playbook.yml -i /etc/ansible/hosts -u ${3}
 }
 
