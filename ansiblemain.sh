9f42fd47a6d2f4969cd3c89c42438dc8b7a3b6fe
sayali0512 <63386141+sayali0512@users.noreply.github.com>
2020-05-18 14:35:26 +0530

    Update ansiblemain.sh

--- ansiblemain.sh
+++ ansiblemain.sh
@@ -8,7 +8,7 @@ echo "decoded text : ${decotext}" >> /home/"${1}"/log.txt
 
 clonerepo(){
   cd /home/"${1}"/ 
-  wget https://raw.githubusercontent.com/sayosh0512/ansibles-wordpress-script/master/wordpressinstall.sh
+  wget https://raw.githubusercontent.com/sayali0512/ansibles-wordpress-script/master/wordpressinstall.sh
   sudo chown -R "${1}":"${1}" /home/"${1}"/wordpressinstall.sh
 }
 clonerepo ${1} >> /home/"${1}"/log.txt
