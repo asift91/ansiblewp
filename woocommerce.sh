8b2992abac79b088d4ae37c10950a20d774eb8c2
asift91 <60592188+asift91@users.noreply.github.com>
2020-05-18 20:50:01 +0530

    Update woocommerce.sh

--- woocommerce.sh
+++ woocommerce.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+
+wooco_path=/home/${2}
+echo "domain_name ${1}" >>${wooco_path}/log.txt
+echo "user_name ${2}" >>${wooco_path}/log.txt
+
 downloadwoocommerce(){
   wget -p https://downloads.wordpress.org/plugin/woocommerce.4.0.1.zip /home/${1}/
 }
 
 extractfile(){
-  echo "domain_name ${1}" >>/home/${2}/log.txt
   sudo apt install unzip
-  sudo unzip /home/${2}/downloads.wordpress.org/plugin/woocommerce.4.0.1.zip
-  sudo cp -rf /home/${2}/woocommerce /var/www/html/wordpress/wp-content/plugins/
-  sudo rm -rf /home/${2}/woocommerce
+  sudo unzip ${wooco_path}/downloads.wordpress.org/plugin/woocommerce.4.0.1.zip
+  sudo cp -rf ${wooco_path}/woocommerce /var/www/html/wordpress/wp-content/plugins/
+  sudo rm -rf ${wooco_path}/woocommerce
 }
 downloadwoocommerce ${2}
 extractfile ${1} ${2} 
