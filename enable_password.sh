710ed142c0220cbdcca0bcaf3619b5c1656a9c0e
asift91 <60592188+asift91@users.noreply.github.com>
2020-05-18 18:23:50 +0530

    Update enable_password.sh

--- enable_password.sh
+++ enable_password.sh
@@ -3,16 +3,16 @@
 #echo "password is ${1}"  >>/home/azureadmin/log.txt
 #Enabling password authentication
 cd /etc/ssh/  >> /home/azureadmin/log.txt
-sudo sed -i "s~PasswordAuthentication no~PasswordAuthentication yes~" /etc/ssh/sshd_config  >> /home/azureadmin/log.txt
-sudo sed -i "s~#UseLogin no~UseLogin yes~" /etc/ssh/sshd_config  >> /home/azureadmin/log.txt
-sudo sed -i "s~#   StrictHostKeyChecking ask~   StrictHostKeyChecking no~" /etc/ssh/ssh_config  >> /home/azureadmin/log.txt
+sudo sed -i "s~PasswordAuthentication no~PasswordAuthentication yes~" /etc/ssh/sshd_config  
+sudo sed -i "s~#UseLogin no~UseLogin yes~" /etc/ssh/sshd_config t
+sudo sed -i "s~#   StrictHostKeyChecking ask~   StrictHostKeyChecking no~" /etc/ssh/ssh_config 
 #   StrictHostKeyChecking ask
 
 
 #Restart sshd
-sudo systemctl restart sshd  >> /home/azureadmin/log.txt
+sudo systemctl restart sshd 
 
 #set the password
-sudo passwd azureadmin >> /home/azureadmin/log.txt
+sudo passwd azureadmin 
 #${1}\n  >> /home/azureadmin/log.txt
 #${1}\n  >> /home/azureadmin/log.txt
